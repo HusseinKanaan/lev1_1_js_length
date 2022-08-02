f0785abf7dc238c35e20d27a05a4dc6d94fafb05
HusseinKanaan <107847813+HusseinKanaan@users.noreply.github.com>
2022-08-02 12:21:14 +0200

    Hussein first commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+let firstName = "Hussein"
+let lastName = "Kanaan"
+
+console.log(firstName.length)
+console.log(lastName.length)
+
+let fullName = (`${firstName} ${lastName}`)
+console.log(fullName)
+
+//oder
+console.log(`${firstName} ${lastName}`)
